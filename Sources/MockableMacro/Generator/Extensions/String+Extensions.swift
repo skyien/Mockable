da8f3b8238d9ddf621dead5e565078e106c7ec18
skyien <V.yuranov@gmail.com>
2024-03-27 14:19:53 +0000

    wip

--- Sources/MockableMacro/Generator/Extensions/String+Extensions.swift
+++ Sources/MockableMacro/Generator/Extensions/String+Extensions.swift
@@ -7,8 +7,15 @@
 
 extension String {
     var capitalizedFirstLetter: String {
-        let firstLetter = prefix(1).capitalized
+        let firstLetter = prefix(1).uppercased()
         let remainingLetters = dropFirst()
         return firstLetter + remainingLetters
     }
 }
+
+extension String {
+    func appending(_ aString: String) -> String {
+        return self + aString
+    }
+}
+
